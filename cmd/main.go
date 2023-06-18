220294f4da06bf6e31416b930dbe007a0541fb3f
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2023-06-18 00:40:54 +0000

    Created simple books api

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/kasamens/go-fiber-api/pkg/books"
+	"github.com/kasamens/go-fiber-api/pkg/common/config"
+	"github.com/kasamens/go-fiber-api/pkg/common/db"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	app := fiber.New()
+	db := db.Init(c.DBUrl)
+
+	books.RegisterRoutes(app, db)
+
+	app.Listen(c.Port)
+}
